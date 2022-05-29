f1e2dd02640b83f8e900a65ddb4d0bab0adff14e
SM-2102 <sukanya.manna.2002@gmail.com>
2022-05-30 01:59:16 +0530

    COMM

---
+++ LAB_7/b.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int length(char a[]);
+int compare(char[], char[]);
+int main()
+{
+    char a1[20], a2[20];
+    printf("Enter two strings: \n");
+    gets(a1);
+    gets(a2);
+    printf("%d",compare(a1,a2));
+    return 0;
+}
+int compare(char a1[], char a2[])
+{
+    int a1_len=length(a1);
+    int a2_len=length(a2);
+    if(a1_len != a2_len)
+    {
+        if(a1_len<a2_len)
+            return -1;
+        if(a1_len>a2_len)
+            return 1;
+    }
+    for(int i = 0; i < a1_len; i++ )
+    {
+        if(a1[i]!=a2[i])
+        {
+            if(a1[i] < a2[i])
+                return -1;
+            if(a1[i] > a2[i])
+                return 1;
+        }
+    }
+    return 0;
+}  
+int length(char a[])
+{
+    int count=0;
+    for(int i=0;a[i]!='\0';i++)
+    count++;
+    return count;
+}
