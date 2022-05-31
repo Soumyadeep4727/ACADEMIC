433050b1fa0bf210f62261a3cbfdab32e0135b54
SM-2102 <sukanya.manna.2002@gmail.com>
2022-06-01 02:15:44 +0530

    comm

--- LAB_7/2.c
+++ LAB_7/2.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int m, n, p, sum=0, a[10][10], b[10][10], c[10][10];
+    int m, n, p, sum=0, a[10][10], b[10][10], c[10][10] = {0};
     printf("Enter the rows and columns of first matrix : ");
     scanf("%d %d", &m, &n);
     printf("Enter the first matrix : \n");
@@ -17,11 +17,8 @@ int main()
     scanf("%d", &b[i][j]);
     for(int i=0;i<m;i++)
         for(int j=0;j<p;j++)
-        {
-            c[i][j] = 0;
             for(int k=0;k<n;k++)
                 c[i][j] += a[i][k]*b[k][j];
-        }
     printf("The matrix after multiplication : \n");
     for(int i=0;i<m;i++)
     {
