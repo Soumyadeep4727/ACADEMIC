3fddcc260f7c57c81d966eed260fd8ae086ff384
SM-2102 <sukanya.manna.2002@gmail.com>
2022-06-10 23:51:01 +0530

    done

--- PPS/07/1.c
+++ PPS/07/1.c
@@ -1,1 +1,28 @@
 /* add two matrices */
+
+#include<stdio.h>
+
+int main()
+{
+    int a[10][10], b[10][10], row, col;
+    printf("Enter the number of rows and columns : ");
+    scanf("%d %d",&row, &col);
+    printf("Enter the first matrix :\n");
+    for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+            scanf("%d", &a[i][j]);
+    printf("Enter the second matrix :\n");
+    for(int i=0;i<row;i++)
+        for(int j=0;j<col;j++)
+        {
+            scanf("%d", &b[i][j]);
+            a[i][j]+=b[i][j];
+        }
+    printf("Matrix 1 + Matrix 2 :\n");
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+}
--- PPS/07/3.c
+++ PPS/07/3.c
@@ -1,1 +1,24 @@
 /* check whether a text is palindrome */
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[20];
+    int flag=0;
+    printf("Enter the string :\n");
+    gets(a);
+    int len = strlen(a);
+    for(int i=0,j=len-1; i<len/2 && j>len/2;i++,j--)
+        if(a[i] != a[j])
+        {
+            flag = 1;
+            break;
+        }
+    if(flag==1)
+        printf("It is not a palindrome.\n");
+    else
+        printf("It is a palindrome.\n");
+
+}
