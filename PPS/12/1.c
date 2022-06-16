c22e71ea442eef7e6fd1aec4f5f23432a05e1e69
SM-2102 <sukanya.manna.2002@gmail.com>
2022-06-17 01:12:37 +0530

    ikik

---
+++ PPS/12/1.c
@@ -0,0 +1,42 @@
+/*  structure containing roll, name, address of several students, 
+    sort in ascending order of roll number and
+    display the sorted students' details */
+
+#include<stdio.h>
+
+struct student
+{
+    int roll;
+    char name[20];
+    char address[100];
+};
+
+int main()
+{
+    int n,i,j,key;
+    struct student s[10],temp;
+    printf("Enter the number of students : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the roll, name and address of student : \n");
+        scanf("%d",&s[i].roll);
+        scanf("%s",s[i].name);
+        scanf("%s",s[i].address);
+    }
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(s[i].roll>s[j].roll)
+            {
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+    printf("The sorted details : \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",s[i].roll);
+        printf("%s\t",s[i].name);
+        printf("%s\n",s[i].address);
+    }
+}
---
+++ PPS/12/2.c
@@ -0,0 +1,2 @@
+/*  union to find the value of y = x^n where x and y are floating point
+    and n is either integer or floating point */
