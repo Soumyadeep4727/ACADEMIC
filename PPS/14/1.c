0135dcf70b5084c1a450f69e4dba3a4c747626dc
SM-2102 <sukanya.manna.2002@gmail.com>
2022-06-24 22:42:55 +0530

    done

--- PPS/14/1.c
+++ PPS/14/1.c
@@ -1,1 +1,19 @@
 /* store a set of integers in input.txt */
+
+#include<stdio.h>
+
+int main()
+{
+    FILE *fpt;
+    int n,num;
+    fpt = fopen("input.txt","w");
+    printf("Enter the number of integers : ");
+    scanf("%d",&n);
+    printf("Enter the numbers : ");
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&num);
+        putw(num,fpt);        
+    }
+    fclose(fpt);
+}
--- PPS/14/2.c
+++ PPS/14/2.c
@@ -1,1 +1,29 @@
 /* read contents of input.txt, sort, and store the sorted numbers in output.txt */
+
+#include<stdio.h>
+
+int main()
+{
+    FILE *fpt1,*fpt2;
+    int n=0,num,i=0,j,key;
+    fpt1 = fopen("input.txt","r");
+    int arr[30];
+    while((num=getw(fpt1))!=EOF)
+    {
+        arr[i] = num;
+        i++;
+        n++;
+    }
+    for(i=1;i<n;i++)
+    {
+        key = arr[i];
+        for(j =i-1;j>=0 && arr[j] > key;j--)
+            arr[j+1]=arr[j];
+        arr[j+1]=key;
+    }
+    fpt2 = fopen("output.txt","w");
+    for(int i=0;i<n;i++)
+        putw(arr[i],fpt2);
+    fclose(fpt1);
+    fclose(fpt2);
+}
