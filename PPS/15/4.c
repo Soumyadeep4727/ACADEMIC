cc8f9cf7a86ba03e1c4c761f8dc8c14129ebc696
SM-2102 <sukanya.manna.2002@gmail.com>
2022-07-02 20:48:54 +0530

    done

--- PPS/15/4.c
+++ PPS/15/4.c
@@ -1,1 +1,51 @@
 /* quick sort */
+
+#include<stdio.h>   
+
+void quicksort(int number[25],int first,int last)
+{
+    int i, j, pivot, temp;
+    if(first<last)
+    {
+        pivot=first;
+        i=first;
+        j=last;
+        while(i<j)
+        {  
+            while(number[i]<=number[pivot]&&i<last)
+                i++;
+            while(number[j]>number[pivot])
+                j--;
+            if(i<j)
+            {
+                temp=number[i];
+                number[i]=number[j];
+                number[j]=temp;
+            }
+        }
+        temp=number[pivot];
+        number[pivot]=number[j];
+        number[j]=temp;
+        quicksort(number,first,j-1);
+        quicksort(number,j+1,last);
+   }
+}
+
+int main()
+{
+    int arr[20],n;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    printf("Enter the values : ");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    printf("The original array : \n");
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+	quicksort(arr, 0, n - 1);
+    printf("The sorted array : \n");
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    printf("\n");  
+}
