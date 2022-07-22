6335c18c32febb421981ad5e19eccff195b7da8d
SM-2102 <sukanya.manna.2002@gmail.com>
2022-07-22 21:46:27 +0530

    ..

--- PYTHON/2/1/z.py
+++ PYTHON/2/1/z.py
@@ -2,5 +2,5 @@
 
 a = complex(input('Enter a complex number : '))
 print('The real part is',a.real)
-print('The real part is',a.imag) 
-print('The real part is',a.conjugate()) 
+print('The imaginary part is',a.imag) 
+print('The conjugate is',a.conjugate()) 
