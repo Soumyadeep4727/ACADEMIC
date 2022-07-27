03bcf23d2659321783ddbb66ec500eeb809b1467
SM-2102 <sukanya.manna.2002@gmail.com>
2022-07-27 10:14:56 +0530

    lol

--- PYTHON/3/3.py
+++ PYTHON/3/3.py
@@ -1,17 +1,19 @@
-# Take score only between 0.0 and 10. 
-# >=9 A, >=8 B, >=7 C, >=6 D, <6 F
+# Take score only between 0 and 100
+# Change it to (0.0, 1.0)
+# >=.9 A, >=.8 B, >=.7 C, >=.6 D, <.6 F
 
-a = float(input("Enter a score between 0 and 10 : "))
-if(a<0 or a>10):
+a = float(input("Enter a score between 0 and 100 : "))
+if(a<0 or a>100):
     print("Wrong Input")
 else:
-    if(a>=9):
+    a = a/100
+    if(a>=.9):
         print('A')
-    elif(a>=8):
+    elif(a>=.8):
         print('B')
-    elif(a>=7):
+    elif(a>=.7):
         print('C')
-    elif(a>=6):
+    elif(a>=.6):
         print('D')
     else:
         print('F')
