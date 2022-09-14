662982338a1e6b2a078443dd28697f9806f96c5d
Soumyadeep Roy <102427795+Soumyadeep4727@users.noreply.github.com>
2022-09-14 12:44:51 +0530

    Update 17.py

--- PYTHON/8/17.py
+++ PYTHON/8/17.py
@@ -1,6 +1,6 @@
 # invert a dictionary, i.e., it makes key of one dictionary value of another and vice versa
 
-dict = {1:'hell',2:'is',3:'empty'}
+dict = {1:'hell',2:'is',3:'full',4:'of',5:'people',6:'like',7:'me'}
 dict_inv={}
 for key,value in dict.items():
     dict_inv[value]=key
